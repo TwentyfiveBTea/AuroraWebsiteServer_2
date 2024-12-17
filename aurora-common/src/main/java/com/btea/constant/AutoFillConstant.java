82e7399fbb05574d3b1d5d5f866c276cab8ae256
TwentyfiveBTea <1248102041@qq.com>
2024-12-17 14:10:40 +0800

    完成实体类字段自动注入代码

---
+++ aurora-common/src/main/java/com/btea/constant/AutoFillConstant.java
@@ -0,0 +1,16 @@
+package com.btea.constant;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2024/12/17 14:05
+ * @Description: 公共字段自动填充相关常量
+ */
+public class AutoFillConstant {
+    /**
+     * 实体类中的方法名称
+     */
+    public static final String SET_CREATE_TIME = "setCreateTime";
+    public static final String SET_UPDATE_TIME = "setUpdateTime";
+    public static final String SET_CREATE_USER = "setCreateUser";
+    public static final String SET_UPDATE_USER = "setUpdateUser";
+}
